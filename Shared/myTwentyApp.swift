974258a2ec9954530f6bec3c66f72d1a152bb9fb
johnhuzhy <johnho560@gmail.com>
2022-04-05 21:28:24 +0900

    Initial Commit

---
+++ Shared/myTwentyApp.swift
@@ -0,0 +1,17 @@
+//
+//  myTwentyApp.swift
+//  Shared
+//
+//  Created by Huzhy on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct myTwentyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
